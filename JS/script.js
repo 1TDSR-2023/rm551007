49f6840d8b75a3c3405670084a14d4e330c4579f
Dericki <derick.araujoo@gmail.com>
2023-05-03 22:56:08 -0300

    button-rgb

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,30 @@
 //}
 
 //console.log(nome1)
+
+let botao = document.getElementById("meu-btn");
+
+botao.addEventListener("click",function(){ 
+   // console.log(this.TextContent)
+   //Função matematica Math
+   //Random = Retorna um numero aleatorio entre  0 e 1 
+   //floor = Ela arredonda um numero pra baixo.
+   //ceil = Ela arredonda um numero pra cima.
+   //round = ele arredonda um numero aleatoriamente.S
+    let r=0,g=0,b=0;
+    r = Math.round(Math.random()*255)
+    g = Math.round(Math.random()*255)
+    b = Math.round(Math.random()*255)
+
+    //this.setAttribute("style","background-color:rgb("+r+","+g+","+b+");");
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+
+});
+
+//ESTUDAR
+//querySelector
+//querySelectorAll
+//Array
+//Estruturas de repetição
+
+   
