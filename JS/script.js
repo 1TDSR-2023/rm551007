eab1226fa462a33e50f9ff0429ff9862e8b03d97
Dericki <derick.araujoo@gmail.com>
2023-05-03 21:43:38 -0300

    adicionando js

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+var nome = "Ruan"
+let nome2 = "Boris"
+
+//REGRA 1 : INICIALIZAR NA DECLARAÇÂO;
+//REGRA 2 : NÂO ALTERAR O SEU VALOR DURANTE O FLUO DE PROGRAMA;
+
+const PI = 3.14;
+
+var nome1 = "petroni"
+
+if (nome1 != ""){
+    let nome1 = "Geovana";
+
+}
+
+console.log(nome1)
